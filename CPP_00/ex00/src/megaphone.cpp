d760bab1cce9bf5079ac087880872e57a2da8f60
Gabriel Tocha <gabriel.tocha@gmail.com>
2023-03-30 14:34:20 +0100

    ex00 done

--- CPP_00/ex00/src/megaphone.cpp
+++ CPP_00/ex00/src/megaphone.cpp
@@ -6,13 +6,24 @@
 /*   By: gda_cruz <gda_cruz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 19:33:27 by gda_cruz          #+#    #+#             */
-/*   Updated: 2023/03/28 19:52:50 by gda_cruz         ###   ########.fr       */
+/*   Updated: 2023/03/30 14:27:17 by gda_cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+using namespace std;
 
-int main()
+int	main(int argc, char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	if (argc == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				cout << (char)toupper(argv[i][j]);
+		}
+	}
+	cout << endl;
 }

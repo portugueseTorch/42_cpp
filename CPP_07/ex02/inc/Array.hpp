57fb04a50ec14d60428b9b14c9914359a7c508da
Gabriel Tocha <gabriel.tocha@gmail.com>
2023-06-13 15:55:43 +0100

    CPP_07 finished

--- CPP_07/ex02/inc/Array.hpp
+++ CPP_07/ex02/inc/Array.hpp
@@ -5,17 +5,52 @@
 # include <iostream>
 # include <exception>
 
+# define debug(x) std::cout << x << std::endl
+
 template <class T>
 class Array
 {
 	public:
-		Array<T>() { _size = 0; };
-		Array<T>(unsigned int size) { _array = new T[size]; };
-		Array<T>(const Array &obj);
-		Array &operator=(const Array &obj);
-		~Array();
+		// CONSTRUCTORS
+		Array<T>() { _size = 0; _array = new T[_size]; };
+		Array<T>(unsigned int size) { _size = size; _array = new T[size]; };
+		Array<T>(const Array &obj) {
+			_size = obj.getSize();
+			_array = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+				_array[i] = obj[i];
+		}
+		Array<T> &operator=(const Array &obj) {
+			if (&obj == this)
+				return *this;
+			delete[] _array;
+			_size = obj.getSize();
+			_array = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+				_array[i] = obj[i];
+			return *this;
+		}
 
-		T &operator[]();
+		// DESTRUCTORS
+		~Array() { delete[] _array; }
+
+		// GETTERS
+		unsigned int getSize() const { return _size; };
+
+		// OPERATORS
+		T &operator[](int index) const {
+			if (index < 0 || static_cast<unsigned int>(index) >= _size)
+				throw InvalidIndexException();
+			return _array[index];
+		}
+
+		// EXCEPTIONS
+		class InvalidIndexException: public std::exception
+		{
+			public:
+				virtual const char* what() const throw() { return "Invalid Index"; }
+		};
+
 	private:
 		T *_array;
 		unsigned int _size;
--- CPP_07/ex02/src/main.cpp
+++ CPP_07/ex02/src/main.cpp
@@ -1,53 +1,89 @@
 #include <iostream>
-#include "../inc/Array.hpp
+#include <cstdlib>
+#include "../inc/Array.hpp"
 
 #define MAX_VAL 750
 int main(int, char**)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	std::cout << "\tTESTING STRING ALLOCATION" << std::endl;
+	{
+		Array<std::string> tracks(5);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+		tracks[0] = std::string("É Só Inveja");
+		tracks[1] = std::string ("Adolfo Dias");
+		tracks[2] = std::string ("O Peixe");
+		tracks[3] = std::string ("Chupa Teresa");
+		tracks[4] = std::string ("Cabritinha");
+		
+		for (int i = 0; i < 5; i++)
+			std::cout << i + 1 << ". " << tracks[i] << std::endl;
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
+	std::cout << "\n\tTESTING POINTER ALLOCATION" << std::endl;
+	{
+		Array<std::string *> tracks(5);
+
+		tracks[0] = new std::string("É Só Inveja");
+		tracks[1] = new std::string ("Adolfo Dias");
+		tracks[2] = new std::string ("O Peixe");
+		tracks[3] = new std::string ("Chupa Teresa");
+		tracks[4] = new std::string ("Cabritinha");
+		
+		for (int i = 0; i < 5; i++)
+			std::cout << i + 1 << ". " << *tracks[i] << "\t\t(" << tracks[i] << ")" << std::endl;
+		
+		for (int i = 0; i < 5; i++)
+			delete tracks[i];
+	}
+
+	std::cout << "\n\tMAIN FROM THE SUBJECT" << std::endl;
+	{
+		Array<int> numbers(MAX_VAL);
+		int* mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		std::cout << "Success: mirror and numbers are equal" << std::endl;
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;//
+	}
     return 0;
 }
